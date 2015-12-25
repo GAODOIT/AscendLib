ef3575886ad0af84d8e4613665d0494d7c58f7ab
gaodong <gaodong@hupu.net>
2015-12-25 10:08:52 +0800

    add test .gitignore

--- GO_TEST/test.go
+++ GO_TEST/test.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-    "time"
-    "fmt"
+	"fmt"
 )
 
+/*
 func main() {
-    /*这是我的第一个程序*/
+    //这是我的第一个程序
     fmt.Println("Hello, World!")
     //var now_time time.Time
     fmt.Println(time.Now())
-
     fmt.Println(time.Now().IsZero())
-    
+
+}
+*/
+func main() {
+
+	var a int
+
+	var f float32
+
+	var str string
+
+	fmt.Print("准备录入数据：")
+
+	fmt.Scanf("%d, %f, %s", &a, &f, &str) //录入数据
+
+	fmt.Println("输出录入结果：", a, f, str)
 }
